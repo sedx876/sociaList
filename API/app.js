43668dfc86c075f78c80487d591a94801584dc8d
sedx876 <swatkins876@gmail.com>
2021-01-18 08:27:02 -0500

    setup routes folder and installed chalk and morgan dependencies

--- API/app.js
+++ API/app.js
@@ -1,12 +1,35 @@
 const express = require('express')
+const morgan = require('morgan')
+const chalk = require('chalk')
 const colors = require('colors')
 
 const app = express()
 
+//Morgan middleware
+const morganMiddleware = morgan(function (tokens, req, res) {
+  return [
+      '\n\n\n',
+      chalk.hex('#ff4757').bold('🍄  Morgan --> '),
+      chalk.hex('#34ace0').bold(tokens.method(req, res)),
+      chalk.hex('#ffb142').bold(tokens.status(req, res)),
+      chalk.hex('#ff5252').bold(tokens.url(req, res)),
+      chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
+      chalk.hex('#f78fb3').bold('@ ' + tokens.date(req, res)),
+      chalk.yellow(tokens['remote-addr'](req, res)),
+      chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
+      chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
+      '\n\n\n',
+  ].join(' ');
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node')
-})
+app.use(morganMiddleware)
+
+
+//Bring in Routes
+const { getPosts } = require('./routes/post')
+
+
+app.get('/', getPosts)
 
 const port = 8080
 
---
+++ API/routes/post.js
@@ -0,0 +1,7 @@
+exports.getPosts = (req, res) => {
+  res.send('Hello from Node')
+}
+
+// module.exports = {
+//   getPosts
+// }
