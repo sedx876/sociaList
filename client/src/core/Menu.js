2b6bc9721b3bed2dc8dd3f2c40160b3f8ac2cd8f
sedx876 <swatkins876@gmail.com>
2021-01-22 06:13:56 -0500

    update on Menu.js

--- client/src/core/Menu.js
+++ client/src/core/Menu.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { signout, isAuthenticated } from '../auth'
+// import { signout, isAuthenticated } from '../auth'
 
-const Menu = () => (
+
+const isActive = (history, path) => {
+  if (history.location.pathname === path) return { color: '#ff9900' }
+  else return { color: '#ffffff' }
+}
+
+export const signout = next => {
+  if (typeof window !== 'undefined') localStorage.removeItem('jwt')
+  next()
+  return fetch('http://localhost:8080/api/signout', {
+      method: 'GET'
+  })
+      .then(response => {
+          console.table('signout', response)
+          return response.json()
+      })
+      .catch(err => console.table(err))
+}
+
+const Menu = ({history}) => (
   <div>
-    <Link to='/'>Home</Link>
-    <Link to='/signin'>Log In</Link>
-    <Link to='/signup'>Create Account</Link>
+    <ul className='nav nav-tabs bg-primary'>
+      <li className="nav-item">
+        <Link className="nav-link" style={isActive(history, '/')} to="/">
+          sociaList
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">
+          Log In
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">
+          Create Account
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <span className="nav-link" 
+          style={{ cursor: 'pointer', color: '#fff' }} 
+          onClick={() => signout(() => history.push('/'))}>
+          Sign Out 
+        </span>
+      </li>
+    </ul>
   </div>
 )
 
-export default Menu
+export default withRouter(Menu)
